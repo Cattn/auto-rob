6205b29d44984576056d28244bd7d91445693bca
Cattn <Logan@cattn.dev>
2026-07-22 21:47:51 -0400

    m3-svelte + basic bottom bar

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,9 +2,20 @@ import tailwindcss from "@tailwindcss/vite";
 import { svelteTesting } from "@testing-library/svelte/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+import { functionsMixins } from "vite-plugin-functions-mixins";
 
 export default defineConfig({
-  plugins: [tailwindcss(), sveltekit()],
+  plugins: [
+    tailwindcss(),
+    sveltekit(),
+    functionsMixins({ deps: ["m3-svelte"] }),
+  ],
+  optimizeDeps: {
+    exclude: ["m3-svelte"],
+  },
+  ssr: {
+    noExternal: ["m3-svelte"],
+  },
   test: {
     projects: [
       {
